d63cef906852973673824165b765635eb3d3386d
NonSpicyBurrito <burritoyangster@gmail.com>
2021-07-27 17:54:08 +0800

    Updated sonolus.js to 2.0.0

--- src/serve.ts
+++ src/serve.ts
@@ -2,14 +2,17 @@ import { serve } from 'sonolus.js'
 
 import { buildOutput } from '.'
 
-serve(buildOutput, {
-    levelCover: {
-        type: 'LevelCover',
-        url:
-            'https://bestdori.com/assets/jp/musicjacket/musicjacket260_rip/assets-star-forassetbundle-startapp-musicjacket-musicjacket260-253_loki-jacket.png',
-    },
-    levelBgm: {
-        type: 'LevelBgm',
-        url: 'https://bestdori.com/assets/jp/sound/bgm253_rip/bgm253.mp3',
-    },
-})
+const sonolus = serve(buildOutput)
+
+const level = sonolus.db.levels[0]
+level.cover = {
+    type: 'LevelCover',
+    hash: '',
+    url:
+        'https://bestdori.com/assets/jp/musicjacket/musicjacket260_rip/assets-star-forassetbundle-startapp-musicjacket-musicjacket260-253_loki-jacket.png',
+}
+level.bgm = {
+    type: 'LevelBgm',
+    hash: '',
+    url: 'https://bestdori.com/assets/jp/sound/bgm253_rip/bgm253.mp3',
+}
