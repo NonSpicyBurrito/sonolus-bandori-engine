d5b07ca5b911be424f871529736e9fc0943c1bf0
NonSpicyBurrito <burritoyangster@gmail.com>
2021-07-31 00:13:32 +0800

    Increased version number to 0.5.0

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,7 +5,7 @@ import { Resource } from './Resource'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const archetypes = require('./archetypes')
 
-export const version = '0.4.1'
+export const version = '0.5.0'
 
 export const engineInfo = {
     name: 'bandori',
